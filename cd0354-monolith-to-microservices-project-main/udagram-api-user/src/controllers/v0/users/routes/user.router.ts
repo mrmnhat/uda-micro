52fdc8ef89cd4168f78073a1115711c9bbdf87bf
mrmnhat <mrmnhat@gmail.com>
2024-06-06 18:21:37 +0700

    update code

--- cd0354-monolith-to-microservices-project-main/udagram-api-user/src/controllers/v0/users/routes/user.router.ts
+++ cd0354-monolith-to-microservices-project-main/udagram-api-user/src/controllers/v0/users/routes/user.router.ts
@@ -8,7 +8,7 @@ const router: Router = Router();
 router.use('/auth', AuthRouter);
 
 router.get('/', async (req: Request, res: Response) => {
-  res.send('ok for testing');
+  res.send('ok');
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
