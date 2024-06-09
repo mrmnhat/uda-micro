1697485f9357961b9800acc35396eaba5866098b
mrmnhat <mrmnhat@gmail.com>
2024-06-09 22:53:33 +0700

    update sql

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=postgres.ce8pfkihmjtf.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres1.ce8pfkihmjtf.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::407229953736-udac
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=j3S7xQHjubYQHrVg63Wb1kwcEnOu2vLKJbd0h4E7
+export JWT_SECRET=emEzNW11IMjz5h97ZASvJBTt7wLJkBR/RX8Lk5cj
 export URL=http://localhost:8100
