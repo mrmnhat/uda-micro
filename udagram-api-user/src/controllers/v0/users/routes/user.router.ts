16d3209b76b2c5274d0fa3250d8cb27dd6881172
mrmnhat <mrmnhat@gmail.com>
2024-06-09 17:16:51 +0700

    update code

--- udagram-api-user/src/controllers/v0/users/routes/user.router.ts
+++ udagram-api-user/src/controllers/v0/users/routes/user.router.ts
@@ -7,9 +7,7 @@ const router: Router = Router();
 
 router.use('/auth', AuthRouter);
 
-router.get('/', async (req: Request, res: Response) => {
-  res.send('ok for ');
-});
+router.get('/');
 
 router.get('/:id', async (req: Request, res: Response) => {
   const {id} = req.params;
